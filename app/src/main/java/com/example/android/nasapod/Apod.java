e1dcc5bf9da29d84bd718e0bb6318c54a91d2aa0
Raul Flores <raulflo2012@gmail.com>
2019-12-07 17:26:26 -0600

    Add Gson dependency and Apod class

---
+++ app/src/main/java/com/example/android/nasapod/Apod.java
@@ -0,0 +1,34 @@
+package com.example.android.nasapod;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Apod {
+
+    @SerializedName("url")
+    private String mApodImage;
+    @SerializedName("title")
+    private String mApodName;
+    @SerializedName("date")
+    private String mApodDate;
+
+
+    public Apod(String aPodImage, String aPodName, String aPodDate){
+
+        this.mApodImage = aPodImage;
+        this.mApodName = aPodName;
+        this.mApodDate = aPodDate;
+
+    }
+
+    public String getApodImage() {
+        return mApodImage;
+    }
+
+    public String getApodName() {
+        return mApodName;
+    }
+
+    public String getApodDate() {
+        return mApodDate;
+    }
+}
