78d947ee38c777ba725743b44cd97528d9e932c8
Raul Flores <raulflo2012@gmail.com>
2019-12-10 15:21:55 -0600

    Add more fakeApods to list and add log.d

--- app/src/main/java/com/example/android/nasapod/ApodAdapter.java
+++ app/src/main/java/com/example/android/nasapod/ApodAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.nasapod;
 
 import android.content.Context;
+import android.icu.text.IDNA;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 
 public class ApodAdapter extends RecyclerView.Adapter<ApodViewHolder> {
 
+    private static final String TAG = "NASAapod";
     private Context mContext;
     private ArrayList<Apod> mApodList;
 
@@ -24,12 +27,16 @@ public class ApodAdapter extends RecyclerView.Adapter<ApodViewHolder> {
     @NonNull
     @Override
     public ApodViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        Log.d(TAG, "onCreateViewHolder");
         View v = LayoutInflater.from(mContext).inflate(R.layout.single_apod, parent, false);
         return new ApodViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ApodViewHolder holder, int position) {
+
+        Log.d(TAG,"onBindViewHolder");
         Apod currentApod = mApodList.get(position);
 
         holder.bind(currentApod);
