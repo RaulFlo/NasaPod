d82b471551074ae120997e80da56c410498df212
Raul Flores <raulflo2012@gmail.com>
2020-06-01 07:29:50 -0500

    Changed margin of the Cardviews

--- app/src/main/java/com/example/android/nasapod/adapter/ApodViewHolder.java
+++ app/src/main/java/com/example/android/nasapod/adapter/ApodViewHolder.java
@@ -9,11 +9,13 @@ import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
 import androidx.palette.graphics.Palette;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.CustomTarget;
 import com.bumptech.glide.request.transition.Transition;
